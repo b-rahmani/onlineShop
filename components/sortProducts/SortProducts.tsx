fe9f1c0db8d81128e23b8a456417d5ab68373ad9
behnam <behnam.rahmani8@gmail.com>
2022-07-04 12:19:58 +0430

    toggle between mobile and desktop in filters and sort , ...

--- components/sortProducts/SortProducts.tsx
+++ components/sortProducts/SortProducts.tsx
@@ -3,35 +3,41 @@ import classes from "./sortProducts.module.scss";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import FilterIcon from "../icons/FilterIcon";
 const SortProducts = () => {
   interface dataSort {
     name: string;
     value: string;
   }
   interface Sort {
-    dataSort?: dataSort[];
-    selectedSort?: string;
+    dataSort: dataSort[];
+    selectedSort: string | string[];
   }
 
-  const [sort, setSort] = useState<Sort>({});
-  const Router = useRouter();
+  const [sort, setSort] = useState<Sort>({ dataSort: [], selectedSort: "" });
 
+  const Router = useRouter();
+  const Query = Router.query;
   useEffect(() => {
-    console.log(Router);
     axios
       .get("/api/allSortings")
-      .then((res) =>
-        setSort({ dataSort: res.data, selectedSort: res.data[0].value })
-      )
+      .then((res) => {
+        if (Query.sort) {
+          return setSort({ dataSort: res.data, selectedSort: Query.sort });
+        } else {
+          return setSort({
+            dataSort: res.data,
+            selectedSort: res.data[0].value,
+          });
+        }
+      })
       .catch((er) => console.log(er));
-  }, []);
+  }, [Query.sort]);
 
   const selectedSortHandler = (value: string) => {
     setSort((prev) => ({ ...prev, selectedSort: value }));
     const hash = Router.asPath.split("#")[1];
     const url = "/";
-    const Query = Router.query;
-    console.log(Router);
 
     Router.replace(
       {},
@@ -44,26 +50,47 @@ const SortProducts = () => {
       { scroll: false }
     );
   };
+
+  let sortName;
+  if (sort.dataSort.length) {
+    let item = sort.dataSort.find((el) => el.value === sort.selectedSort);
+    sortName = item?.name;
+  }
+
   return (
-    <div className={classes.sortSec}>
-      <p className={classes.sortIcon}>
-        <SortIcom />
-        <span> مرتب سازی :</span>
-      </p>
-      <ul className={classes.sorting}>
-        {sort?.dataSort?.map((item) => (
-          <li
-            key={item.name}
-            className={`${classes.sortItem} ${
-              sort.selectedSort === item.value ? classes.active : ""
-            }`}
-            onClick={() => selectedSortHandler(item.value)}
-          >
-            {item.name}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className={classes.filterSortPhone}>
+        <div className={classes.filter}>
+          <FilterIcon /> <span>فیلتر</span>
+        </div>
+        <div
+          className={classes.sort}
+          onClick={() => setIsDisplayMobileSort(true)}
+        >
+          <SortIcom />
+          <span>{sortName}</span>
+        </div>
+      </div>
+      <div className={classes.sortSec}>
+        <p className={classes.sortIcon}>
+          <SortIcom />
+          <span> مرتب سازی :</span>
+        </p>
+        <ul className={classes.sorting}>
+          {sort?.dataSort?.map((item) => (
+            <li
+              key={item.name}
+              className={`${classes.sortItem} ${
+                sort.selectedSort === item.value ? classes.active : ""
+              }`}
+              onClick={() => selectedSortHandler(item.value)}
+            >
+              {item.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 export default React.memo(SortProducts);
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     deviceSizes: [640, 900, 1500],
     imageSizes: [16, 30, 60],
