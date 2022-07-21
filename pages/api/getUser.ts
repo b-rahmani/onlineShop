5e1e6008a7e59e8cc493c4061e0aebfe0e006c25
behnam <behnam.rahmani8@gmail.com>
2022-07-21 12:21:19 +0430

    success recive data from mongo atlas

--- pages/api/getUser.ts
+++ pages/api/getUser.ts
@@ -1,17 +1,39 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { MongoClient } from "mongodb";
 
-import { dbConnect } from "../../utils/db-connect";
+// import { dbConnect } from "../../utils/db-connect";
+
 
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: any
 ) {
 
 //db connect
-const data=await dbConnect();
+const url=`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@online-shop.ps6bz.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(url);
+const dbName="loginData";
 
-    res.status(200).json({data});
-  
+async function main() {
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('users');
+  const findResult = await collection.find({}).toArray();
+
+  console.log(findResult)
+  return findResult;
+}
+main()
+  .then(users=>{
+    // console.log(users);
+    res.status(200).json(users)
+  })
+  .catch(er=>res.status(500).json({er:er}))
+  .finally(() => client.close());
+
+    // res.status(200).json({data});
+    
 
 }
