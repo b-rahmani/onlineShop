391221186179ef9a1e5a400bdf5d32be566f9de2
behnam <behnam.rahmani8@gmail.com>
2022-08-13 12:07:39 +0430

    add product id paths in mongodb getstatic paths

--- pages/product/[productID]/index.tsx
+++ pages/product/[productID]/index.tsx
@@ -6,6 +6,8 @@ import { GetStaticProps } from "next";
 
 import { vercelClient } from "../../../utils/axios";
 import { allProductsMock } from "../../../filterData/fakeProductData";
+import { MongoClient } from "mongodb";
+import { Db } from "../../../utils/db-connect";
 
 interface SingleProductType {
   product: productType;
@@ -29,49 +31,102 @@ export const getStaticProps = async ({ params }: { [key: string]: any }) => {
   //   `/api/product/${params?.productID}`
   // );
 
+  // try {
+  //   // const { data: allProduct } = await vercelClient.get("/api/allData");
+
+  //   // const product = await allProduct?.find(
+  //   //   (p: productType) => p.id == params?.productID
+  //   // );
+
+  //   const allProduct = allProductsMock;
+  //   const product = await allProduct?.find(
+  //     (p: productType) => p.id == params?.productID
+  //   );
+
+  //   return {
+  //     props: {
+  //       product: product,
+  //       // related: allProduct,
+  //     },
+  //     revalidate: 10,
+  //   };
+  // } catch (error) {
+  //   console.log(error);
+  // }
+  //
+
+  const url = `mongodb+srv://${Db.USER}:${Db.PASSWORD}@cluster0.zisqoh9.mongodb.net/?retryWrites=true&w=majority`;
+  const client = new MongoClient(url);
+  const dbName = Db.DB_NAME;
+
   try {
-    // const { data: allProduct } = await vercelClient.get("/api/allData");
+    await client.connect();
+    const db = client.db(dbName);
+    const collection = db.collection("allProducts");
 
-    // const product = await allProduct?.find(
-    //   (p: productType) => p.id == params?.productID
-    // );
+    let findResult;
 
-    const allProduct = allProductsMock;
-    const product = await allProduct?.find(
+    findResult = await collection.find({}).toArray();
+    // }
+    client.close();
+    console.log(findResult);
+    const converted = await findResult.map((item: any) => ({
+      ...item,
+      _id: item._id.toString(),
+    }));
+
+    const product = await converted?.find(
       (p: productType) => p.id == params?.productID
     );
-
     return {
       props: {
         product: product,
-        // related: allProduct,
+        related: converted,
       },
       revalidate: 10,
     };
-  } catch (error) {
-    console.log(error);
+  } catch (er) {
+    client.close();
+
+    return {
+      props: {
+        err: "some problem",
+      },
+    };
   }
 
-  // } else {
-  //   return {
-  // //     // notFound: true,
-  //     props: {
-  //       status: 404,
-  //     },
-  //   };
-  // }
+  //
 };
 export const getStaticPaths = async () => {
-  try {
-    // const { data } = await vercelClient.get("/api/allData");
-    // const appIds = await data?.map((el: productType) => ({
-    //   params: { productID: el.id.toString() },
-    // }));
+  // try {
+  // const { data } = await vercelClient.get("/api/allData");
+  // const appIds = await data?.map((el: productType) => ({
+  //   params: { productID: el.id.toString() },
+  // }));
 
-    const appIds = [
-      { params: { productID: "1" } },
-      { params: { productID: "2" } },
-    ];
+  const url = `mongodb+srv://${Db.USER}:${Db.PASSWORD}@cluster0.zisqoh9.mongodb.net/?retryWrites=true&w=majority`;
+  const client = new MongoClient(url);
+  const dbName = Db.DB_NAME;
+
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+    const collection = db.collection("allProducts");
+
+    let findResult;
+
+    findResult = await collection.find({}).toArray();
+    // }
+    client.close();
+
+    const appIds = await findResult.map((item: any) => ({
+      params: { productID: item.id.toString() },
+    }));
+
+    // const appIds = [
+    //   { params: { productID: "1" } },
+    //   { params: { productID: "2" } },
+    // ];
 
     // if (data && appIds) {
     return {
